5e3d213dc5d0a83971e6410bf78f2381e6ec27d2
Nicolas <nmolanog@gmail.com>
2019-11-26 09:36:41 -0500

    added script generator for loading data.

--- create_project.R
+++ create_project.R
@@ -1,8 +1,8 @@
 rm(list=ls())
-where<-"/media/nicolas/DATOS/Rprojects"
+where<-"/home/nicolas/Nicolas/rprojects"
 setwd(where)
 
-name_of_newproject<-"PAI_in_AITD"
+name_of_newproject<-"test_proj"
 dir.create(name_of_newproject)
 
 setwd(paste0("./",name_of_newproject))
@@ -17,3 +17,56 @@ dir.create("raw")
 dir.create("Rdata")
 
 setwd(where)
+
+load_from_raw <- 
+"#######################
+###load data
+#######################
+rm(list=ls())
+options(max.print=999999)
+library(pacman)
+p_load(here)
+p_load(openxlsx)
+p_load(tidyverse)
+library(bueri)
+oldir<-getwd()
+
+####see available xlsx files to load
+list.files(\"../data/raw\")%>%str_subset(\".xlsx\")
+###asign the apropiate name file. without xlsx extencion
+file_nm<-\"XXXX\"
+###load file
+wb <- paste0(\"../data/raw/\",file_nm,\".xlsx\")
+getSheetNames(wb)
+####load data
+z0<-read.xlsx(wb, sheet =1, colNames = TRUE,na.strings = \"NA\")
+####load dictionary
+choix<-read.xlsx(wb, sheet =2, colNames = TRUE,na.strings = \"NA\")
+###remove conection
+remove(\"wb\")
+
+###review choix
+colnames(choix)
+colnames(choix)<-c(\"var\",\"type\",\"cat\",\"Def\",\"outcome\",\"groups\")
+####compare names in choix and in colnames of z0
+tempcn<-data.frame(colnames(z0),choix$var,stringsAsFactors =F)
+tempcn%>%filter(colnames.z0.!=choix.var)
+
+###if minor diferences fix that
+choix$var<-colnames(z0)
+###search for duplicated names
+choix$var%>%table()->a
+a[a>1]  
+
+###verify available types
+choix$type%>%table
+
+###create folder if it is not already created
+if(!\"../data/Rdata\" %in% list.dirs(path=\"..\")){
+  dir.create(\"../data/Rdata\")
+}
+
+###save files as RData
+save(z0,choix,file=paste0(\"../data/Rdata\",\"/\",\"MO_ER.RData\"))
+setwd(oldir)"
+writeLines(load_from_raw, paste0(where,"/",name_of_newproject,"/","/R/Load_from_raw.R"))
